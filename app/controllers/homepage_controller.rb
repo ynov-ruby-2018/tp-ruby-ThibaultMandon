fef30281f5afabf48562c64d0c6b687d237adb20
ThibaultMandon <thibault.mandon43@gmail.com>
2018-11-07 16:41:22 +0100

    première version basique affichage article

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,14 @@
+class HomepageController < ApplicationController
+
+  def index
+    @comments = Comment.all.order('created_at DESC')
+  end
+
+  def create
+    Comment.create!(
+     content: params[:comment]
+    )
+
+    redirect_to homepage_index_path
+  end
+end
