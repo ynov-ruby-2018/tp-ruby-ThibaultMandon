42c2e5b10ca625dc73f88a9b9546b104c1958e5a
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 18:34:40 +0100

    Update 20181107153148_create_comments.rb

--- db/migrate/20181107153148_create_comments.rb
+++ db/migrate/20181107153148_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.timestamps
       t.text :content
+      # Manque la clé étrangère user_id
     end
   end
 end
